e3bfc8d42fdeb71563290cfb0deeea167e44b4a2
Lyhour <kouchlyhour@gmail.com>
2026-03-18 20:16:58 +0700

    chore: remove unnecessary comments from base provider.

--- lib/app/core/services/base_provider.dart
+++ lib/app/core/services/base_provider.dart
@@ -2,8 +2,6 @@ import 'package:get/get.dart';
 import '../../config/flavor_config.dart';
 import 'storage_service.dart';
 
-/// Base provider class with common HTTP client configuration.
-/// All API providers should extend this class to avoid code duplication.
 abstract class BaseProvider extends GetConnect {
   @override
   void onInit() {
@@ -17,9 +15,6 @@ abstract class BaseProvider extends GetConnect {
         request.headers['Authorization'] = 'Bearer $token';
       }
       request.headers['Accept'] = 'application/json';
-      // Don't override Content-Type - let GetX set it automatically
-      // For FormData it will set multipart/form-data with boundary
-      // For regular requests it will set application/json
       return request;
     });
 
